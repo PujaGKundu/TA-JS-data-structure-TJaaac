a5bd2a49ceeb63a6405baf06e0700a181296453d
PujaGKundu <pujakundu100@gmail.com>
2021-10-25 12:41:48 +0530

    Assignment: Operation on Arrays

--- block-BJaace/code/index.js
+++ block-BJaace/code/index.js
@@ -20,7 +20,7 @@ console.log(colors[2]);
 
 // 6. Access the last color in the array and print it to the console using `console.log()`
 
-console.log(colors[4]);
+console.log(colors[colors.length - 1]);
 
 // 7. Write one line of code that changes the value of the first color in the list to "black" (overwriting the previous value).
 
@@ -28,7 +28,7 @@ colors[0] = 'Black';
 
 // 8. Write one line of code that changes the value of the last color in the list to "ultraviolet" (overwriting the previous value).
 
-colors[4] = "ultraviolet";
+colors[colors.length - 1] = "ultraviolet";
 
 // 9. Now, Find the length of the array named `colors`.
 
@@ -44,7 +44,7 @@ colors[colors.length] = 'Violet';
 
 // 12. Add another color to the beginning of the list (use array method array.unshift('gray'))
 
-colors.unshift = 'Gray';
+colors.unshift('Gray');
 
 // 13. Remove the first color from the starting of the array
 
@@ -52,13 +52,13 @@ delete colors[0];
 
 // 14. Remove the last color from the end of list, and then print the length of the array to the console one more time.
 
-delete colors.length;
+delete colors[colors.length - 1];
 
 colors.length;
 
 // 15. Remove the second color from the end of list.
 
-delete colors[1];
+delete colors[colors.length - 2];
 
 // 16. Write a for loop to iterate through every element in the array and print each element to the console.
 
@@ -69,7 +69,7 @@ for (let i = 0; i < colors.length; i++) {
 // 17. Copying from that loop you just wrote, modify it to print every color's value and every color's index in this format: 3, purple or 0, blue etc.
 
 for (let i = 0; i < colors.length; i++) {
-    console.log(i +' , '+ colors[i]);
+    console.log(i +': '+ colors[i]);
 }
 
 // 18. Write a for..of loop to iterate through every element in the array and print each element to the console.
@@ -80,7 +80,7 @@ for (let color of colors) {
 
 // 19. Create a variable named lastColor that will always point to the last element of the colors array, no matter how many colors are in the list. (Hint: make use of the array's length property for this!)
 
-let lastColor = colors[colors.length];
+let lastColor = colors[colors.length - 1];
 
 // 20. Add a new color `tomato` to the index 45. And check the length of the array. Use `console.log` to print.
 
